61a0578c1d53ef087decc676886d4729da048f90
Runxuan Shu <shurunxuan@hotmail.com>
2019-02-20 15:54:00 -0500

    Updated documentation

--- src/DSEngine/DSEngineTestProject/TestGameApp.h
+++ src/DSEngine/DSEngineTestProject/TestGameApp.h
@@ -1,10 +1,42 @@
+/**
+ * @file TestGameApp.h
+ * @author Victor Shu
+ * @brief This file contains the example/test class for 
+ * the game that is using DSEngine.
+ * 
+ * @version 0.1
+ * @date 2019/02/20
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #pragma once
 #include "DSEngineApp.h"
 
+/**
+ * @class TestGameApp
+ * 
+ * @brief Derived from DSEngineApp. 
+ * 
+ * @todo This class should contain actual game logic.
+ * 
+ */
 class TestGameApp :
 	public DSEngineApp
 {
 public:
+	/**
+	 * @brief Override the base class Init function.
+	 * Should initialize game-specific things.
+	 * 
+	 * @param hInstance The instance of the app.
+	 * @param lpCmdLine The command line parameters of the app.
+	 * @param hWnd The window handle.
+	 * @param screenWidth Screen width.
+	 * @param screenHeight Screen height.
+	 * @return true Initialization succeeded.
+	 * @return false Initialization failed.
+	 */
 	bool Init(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND hWnd, int screenWidth, int screenHeight) override;
 };
 
