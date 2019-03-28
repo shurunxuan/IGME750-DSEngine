a7e137b2ca2ff85aa24ffdd39b105835da2a499a
Travis CI <travis@travis-ci.org>
2019-03-28 03:16:18 +0000

    Deploy code docs to GitHub Pages Travis build: 80
    Commit: fe6e4e80a43feff39d432d657031b1b0a467636d

--- html/search/enums_0.js
+++ html/search/enums_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['dsjoystickaxiscode',['DSJoystickAxisCode',['../_d_s_f_x_input_8h.html#a3c5aff8483ea17ad74ce8627f7e95516',1,'DSFXInput.h']]],
-  ['dsjoystickbuttoncode',['DSJoystickButtonCode',['../_d_s_f_x_input_8h.html#a76c1aaf7075123e966502ec3c69082e4',1,'DSFXInput.h']]]
+  ['dsaxiscode',['DSAxisCode',['../_d_s_f_x_input_8h.html#a22550c2bcce3b5808bd63017d726eb84',1,'DSFXInput.h']]],
+  ['dsbuttoncode',['DSButtonCode',['../_d_s_f_x_input_8h.html#a7e6fbdd3adeec44f31d384f2abed16cf',1,'DSFXInput.h']]]
 ];
--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['readframe',['ReadFrame',['../class_d_s_f_f_fmpeg.html#aa4f067a948b598fa09ac42ff6e522558',1,'DSFFFmpeg']]],
-  ['registerinput',['RegisterInput',['../class_d_s_s_input.html#a2d5b43a469249503e763faecd4014ae4',1,'DSSInput']]],
   ['removecomponent',['RemoveComponent',['../class_object.html#a72d9b8149bd79e37a5f3985ee1bd922c',1,'Object']]],
   ['removelightat',['RemoveLightAt',['../class_scene.html#a02b82ebaaee69b1cba47b00cb839ae8c',1,'Scene']]],
   ['render',['Render',['../class_d_s_f_direct3_d.html#a0fb93a73e7f3fceb6c334fa0e31b4219',1,'DSFDirect3D']]],
