3d7fc3fed36d9973abfcfb1c1cef545d41a06828
Travis CI <travis@travis-ci.org>
2019-04-18 14:31:47 +0000

    Deploy code docs to GitHub Pages Travis build: 126
    Commit: 98a71088ee8c4d15641e8c6e89ccee80a90f8b3f

--- html/search/classes_c.js
+++ html/search/classes_c.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['scene',['Scene',['../class_scene.html',1,'']]],
-  ['skybox',['Skybox',['../class_skybox.html',1,'']]]
+  ['skybox',['Skybox',['../class_skybox.html',1,'']]],
+  ['ssaomaterial',['SSAOMaterial',['../class_s_s_a_o_material.html',1,'']]],
+  ['ssaoparameters',['SSAOParameters',['../struct_s_s_a_o_material_1_1_s_s_a_o_parameters.html',1,'SSAOMaterial']]]
 ];
