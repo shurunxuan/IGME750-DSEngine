53dd20b69c1991fd63855071a5c9790475cfd0be
Travis CI <travis@travis-ci.org>
2019-05-22 03:15:51 +0000

    Deploy code docs to GitHub Pages Travis build: 164
    Commit: 799c0c64b223fceb155e23912efaee90c7795c2a

---
+++ html/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghilmnoprstuvw~",
+  1: "abcdfgilmoprstuv",
+  2: "acdlmopst",
+  3: "acdefgilmoprstu~",
+  4: "abcdfhilmnoprstuvw",
+  5: "d",
+  6: "o",
+  7: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "related",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Friends",
+  7: "Pages"
+};
+
